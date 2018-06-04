a171765640eba12fddf98a4423cdbfdcfd812b4a
Fmo <fmo080308@hotmail.com>
2018-06-04 11:31:03 -0700

    Edit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 // Imports dependencies and set up http server
 const
-  PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+  PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN,
   express = require('express'),
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json()); // creates express http server
