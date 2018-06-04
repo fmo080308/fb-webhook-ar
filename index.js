b71ce57ca1f2aba6269222da2a4b39013e1d892d
Fmo <fmo080308@hotmail.com>
2018-06-04 14:31:52 -0700

    Update

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.post('/webhook', (req, res) => {
 });
 
 // Adds support for GET requests to our webhook
-app.get('/webhook', (req, res) => {
+app.get('/webhook', (req, res, next) => {
 
   // Your verify token. Should be a random string.
   let VERIFY_TOKEN = "skyvu_ar";
