5abf98a5be5136fca2ee985d024e4817e713fda2
fmo080308 <fmo080308@hotmail.com>
2018-06-18 10:29:38 +0700

    Update

---
+++ cloud-app-info.js
@@ -0,0 +1,9 @@
+﻿
+var AppInfo = {
+//	Wss: true,
+    AppId: "56bf3bfa-ea12-4be5-825d-3b41316c49d8",
+    AppVersion: "1.0",
+//	MasterServer: "localhost:9090"
+//    FbAppId: "you fb app id", 
+}
+
