67a3bf961c532f195da4dda1abc48130c19f5e6d
fmo080308 <fmo080308@hotmail.com>
2018-08-07 14:34:29 +0700

    Update

--- index.js
+++ index.js
@@ -55,9 +55,9 @@ function LoginCallback(error, result) {
 
 function GetNewsCallback(error, result) {
     if (result !== null) {
-        console.log(result.data.News);
+        console.log(result.data.News[0].Body);
 
-         var data = result.data.News[0];
+        var data = result.data.News[0].Body;
 
         _title = data.title;
         _content = data.content;
