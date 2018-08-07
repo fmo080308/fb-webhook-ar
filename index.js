454c5ffcd59119ac11781d946bc58d623ee85c03
fmo080308 <fmo080308@hotmail.com>
2018-08-07 10:36:46 +0700

    Update

--- index.js
+++ index.js
@@ -5,19 +5,7 @@
 var  express = require('express');
 var  bodyParser = require('body-parser');
 var  request = require('request');
-var  expressWs = require('express-ws');
-var  expressWs = expressWs(express());
-var  app = expressWs.app;
-var socketArray = [];
 
-
-  
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-
-app.use(express.static('public'));
-
-var  aWss = expressWs.getWss('/');
 
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {  
@@ -41,13 +29,6 @@ app.post('/webhook', (req, res) => {
     res.status(200).send('EVENT_RECEIVED');
   } else {
     // Returns a '404 Not Found' if event is not from a page subscription
-	
-	console.log(socketArray.length);
-	var i;
-	for(i = 0; i < socketArray.length;i++)
-	{
-		socketArray[i].send("REPLY");
-	}
 	
     res.sendStatus(404);
   }
@@ -84,16 +65,6 @@ app.get('/webhook', (req, res, next) => {
   else console.log('ERROR');
 }
 );
-
-app.ws('/', function(ws, req) {
-  ws.on('message', function(msg) {
-    console.log(msg);
-	ws.send("HELLO BACK");
-  });
-  console.log("CONNECTED");
-  
-  socketArray.push(ws);
-});
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
